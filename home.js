74f3065eb0c64fc167e8da9da164061d33c7531f
fikri-feb-1 <150456253+fikri-feb-1@users.noreply.github.com>
2026-06-21 10:29:58 +0700

    Add 'Rizky' to DAFTAR_KARYAWAN list

--- home.js
+++ home.js
@@ -20,7 +20,7 @@ function formatRupiahInput(el) {
 }
 
 // ================= DATA KARYAWAN =================
-const DAFTAR_KARYAWAN = ["Esa", "Fikri", "Gata", "Iqbal", "Aldi"];
+const DAFTAR_KARYAWAN = ["Esa", "Fikri", "Gata", "Iqbal", "Aldi", "Rizky"];
 
 function renderKaryawanChecklist() {
   const container = document.getElementById("karyawanContainer");
